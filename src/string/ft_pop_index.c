570c99e60a164f0ff577643974d0f2e4573af99c
Nabil Basheer Ahamed <nbasheer@bess-f4r1s12.clusters.42paris.fr>
2024-12-11 12:19:18 +0100

    feat:ft_pop_index

---
+++ src/string/ft_pop_index.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pop_index.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbasheer <nbasheer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 12:18:14 by nbasheer          #+#    #+#             */
+/*   Updated: 2024/12/11 12:18:28 by nbasheer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**ft_pop_index(char **tab, int index)
+{
+	char	**new_tab;
+	int		i;
+	int		j;
+
+	if (!tab || index < 0 || index >= ft_tabsize(tab))
+		return (tab);
+	new_tab = (char **)malloc(sizeof(char *) * ft_tabsize(tab));
+	if (!new_tab)
+		return (NULL);
+	i = -1;
+	j = -1;
+	while (tab[++i])
+	{
+		if (i == index)
+			continue ;
+		new_tab[++j] = ft_strdup(tab[i]);
+	}
+	new_tab[++j] = NULL;
+	ft_tabfree(tab);
+	return (new_tab);
+}
